adbf25eaee2bfbbda53db2a9e68d3a3c17994da1
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-12 12:01:02 -0300

    Update init-script_rh.sh

--- init-script_rh.sh
+++ init-script_rh.sh
@@ -6,3 +6,4 @@ echo "export PATH=$PATH:/root/.local/bin" >> /root/.bashrc
 
 $(date > /tmp/date.txt)
 echo "RedHat" >> /tmp/date.txt
+cat /root/.ssh/authorized_keys | cut -c 167- > /root/.ssh/authorized_keys
